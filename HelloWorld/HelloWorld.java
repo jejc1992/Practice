4c9cde6ab012ef52e7a705cd13550a6fdcc5389e
Jamie Cross <james.cross@thetestpeople.com>
2016-01-07 16:54:54 +0000

    Hello world

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of HelloWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HelloWorld {
+    public void hello (){
+        System.out.println ("Hello World");
+    }
+
+}
